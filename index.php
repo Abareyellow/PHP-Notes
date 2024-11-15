ea537f28769150408312d962a89ba1385c25587a
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-11-15 20:41:16 +0000

    Start my journey of  php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+    </head>
+    <body>
+    <?php
+    // Single Line comment: Hello World!
+      echo 'Hello World!';
+    ?>
+
+    <?='Hello World'?>
+
+    <?php
+      print 'Hello World'
+    ?>
+    </body>
+</html>
---
+++ quotes.php
@@ -0,0 +1,5 @@
+<?php 
+  $name = 'Joe';
+  echo "Hello, $name";
+  echo 'Hello' . $name . '!';
+?>
